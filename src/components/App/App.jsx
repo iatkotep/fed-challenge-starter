d17f507ac7f9e34a4e556bc22aa4bfa08655c7fd
David Lawson <david@threesig.com>
2021-02-27 13:30:15 -0700

    load classlist

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
-import { Container } from './ui';
+import ClassCards from '../ClassCards/ClassCards';
+
 import { useStateManager } from './hooks/useStateManager';
+import { Container } from './ui';
+import { classes } from './mock';
+
 function App() {
   const {selectedClassIndeces, toggleSelectedClass} = useStateManager();
 
   return (
     <Container>
-
+      <ClassCards {...{classes}} />
     </Container>
   );
 }
