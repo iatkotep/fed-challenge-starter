d3f88d78443281604dfdf7c1e6952fa287b427db
David Lawson <david@threesig.com>
2021-02-27 11:34:34 -0700

    begin definition of mock data

---
+++ src/components/App/mock.js
@@ -0,0 +1,26 @@
+export const classes = [
+  {
+    name: '20-minutes-to-toned',
+  },
+  {
+    name: 'charles-race',
+  },
+  {
+    name: 'full-body-hiit',
+  },
+  {
+    name: 'kafue-river',
+  },
+  {
+    name: 'lake-inniscarra',
+  },
+  {
+    name: 'performance-series',
+  },
+  {
+    name: 'shred-and-burn',
+  },
+  {
+    name: 'slow-pulls',
+  }
+]
